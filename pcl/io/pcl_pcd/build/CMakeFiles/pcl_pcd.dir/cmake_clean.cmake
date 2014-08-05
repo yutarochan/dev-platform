aeeb10715216d5c4c9268fb15ea21dc9479ec9f2
tom <tomstlaurent.mail@gmail.com>
2014-08-04 21:11:19 -0700

    implemented pcl_pcd

---
+++ pcl/io/pcl_pcd/build/CMakeFiles/pcl_pcd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pcl_pcd.dir/pcl_pcd.cpp.o"
+  "pcl_pcd.pdb"
+  "pcl_pcd"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pcl_pcd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
